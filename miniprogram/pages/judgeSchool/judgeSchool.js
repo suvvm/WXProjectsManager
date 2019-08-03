5fa21bb4ae1c5cf4015fe59e352257f2b5579b1c
suvvm <915269675@qq.com>
2019-08-03 19:32:17 +0800

    judgeSchool completed

--- miniprogram/pages/judgeSchool/judgeSchool.js
+++ miniprogram/pages/judgeSchool/judgeSchool.js
@@ -1,18 +1,153 @@
 // pages/judgeSchool/judgeSchool.js
+const app = getApp();
+const db = wx.cloud.database();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    applyid: '',
+    apply: {}
+  },
+  accepted: function () {
+    wx.showLoading({
+      title: '处理中',
+      mask: true,
+      success: (result)=>{
+        
+      },
+      fail: ()=>{},
+      complete: ()=>{}
+    });
+    db.collection('isApproval').where({
+      applyid: this.data.applyid
+    }).get().then(res => {
+      console.log(res);
+      if(res.data.length == 0){
+        db.collection('isApproval').add({  
+          data: {
+            applyid: this.data.applyid,
+            isDepartmentAccept: false,
+            isSchoolAccepted: true
+          }
+        }).then(res2 => {
+          wx.hideLoading();
+          console.log("添加审核信息成功")
+          wx.navigateBack({
+            delta: 1
+          });
+        }).catch(err2 => {
+          console.log("添加审核信息败亡")
+          wx.hideLoading();
+          console.log(err2);
+        })
+      }else{
+        db.collection('isApproval').doc(
+          res.data[0]._id).update({
+            data: {
+              isSchoolAccepted: true,
+            }
+          }).then(res3 => {
+            console.log("修改审核信息成功")
+            wx.navigateBack({
+              delta: 1
+            });
+            wx.hideLoading();
+          }).catch(err3 => {
+            console.log("添加审核信息败亡")
+            console.error(err3);
+            wx.hideLoading();
+          })
+      }
+    }).catch(err => {
+      console.log("查找时出现错误");
+      console.error(err);
+    })
   },
 
+  denied: function () {
+    wx.showLoading({
+      title: '处理中',
+      mask: true,
+      success: (result)=>{
+        
+      },
+      fail: ()=>{},
+      complete: ()=>{}
+    });
+    db.collection('isApproval').where({
+      applyid: this.data.applyid
+    }).get().then(res => {
+      console.log(res);
+      if(res.data.length == 0){
+        db.collection('isApproval').add({  
+          data: {
+            applyid: this.data.applyid,
+            isDepartmentAccept: false,
+            isSchoolAccepted: false
+          }
+        }).then(res2 => {
+          wx.hideLoading();
+          console.log("添加审核信息成功")
+          wx.navigateBack({
+            delta: 1
+          });
+        }).catch(err2 => {
+          console.log("添加审核信息败亡")
+          wx.hideLoading();
+          console.log(err2);
+        })
+      }else{
+        db.collection('isApproval').doc(
+          res.data[0]._id).update({
+            data: {
+              isSchoolAccepted: false,
+            }
+          }).then(res3 => {
+            console.log("修改审核信息成功")
+            wx.navigateBack({
+              delta: 1
+            });
+            wx.hideLoading();
+          }).catch(err3 => {
+            console.log("添加审核信息败亡")
+            console.error(err3);
+            wx.hideLoading();
+          })
+      }
+    }).catch(err => {
+      console.log("查找时出现错误");
+      console.error(err);
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.showLoading({
+      title: '加载中',
+      mask: true,
+      success: (result) => {
 
+      },
+      fail: () => { },
+      complete: () => { }
+    });
+    this.setData({
+      applyid: options.applyid
+    });
+    db.collection('applyinf').doc(
+      options.applyid
+    ).get().then(res => {
+      this.setData({
+        apply: res.data
+      })
+      wx.hideLoading();
+    }).catch(err => {
+      console.error(err);
+      wx.hideLoading();
+    })
   },
 
   /**
