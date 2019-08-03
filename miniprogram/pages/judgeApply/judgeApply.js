aa4062a13e854e0af96b2a1a9cc5751b5e6963f7
suvvm <915269675@qq.com>
2019-08-03 16:53:34 +0800

    page judgeApply completed

--- miniprogram/pages/judgeApply/judgeApply.js
+++ miniprogram/pages/judgeApply/judgeApply.js
@@ -1,19 +1,64 @@
 // pages/judgeApply/judgeApply.js
+const app =  getApp();
+const db = wx.cloud.database();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    openid: '',
+    applys: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.showLoading({
+      title: '加载中',
+      mask: true,
+      success: (result)=>{
+        
+      },
+      fail: ()=>{},
+      complete: ()=>{}
+    });
+    this.setData({
+      openid: app.globalData.openid,
+    })
+    db.collection('applyinf').get().then(res => {
+      this.setData({
+        applys: res.data
+      })
+      wx.hideLoading();
+    }).catch(err =>{
+      console.err(err);
+      wx.hideLoading();
+    })
   },
+  judgeDepartmentFile: function (){
+    wx.navigateTo({
+      url: `../judgeSchool/judgeSchool?applyid=${e.target.dataset.applyid}`,
+      success: (result)=>{
+        
+      },
+      fail: ()=>{},
+      complete: ()=>{}
+    });
+  },
+  judgeSchoolFile: function (){
+    wx.navigateTo({
+      url: `../judgeDepartment/judgeDepartment?applyid=${e.target.dataset.applyid}`,
+      success: (result)=>{
+        
+      },
+      fail: ()=>{},
+      complete: ()=>{}
+    });
+  },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
